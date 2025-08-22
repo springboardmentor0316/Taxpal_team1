00492698ddff5de879c1cd17d687a3ac4ecf2376
AnushaKummari <anushakummari86@gmail.com>
2025-08-22 22:52:08 +0530

    Added react app to frontend project

---
+++ frontend/Taxpal/src/App.jsx
@@ -0,0 +1,11 @@
+import SignIn from './Sign in.jsx';
+import './App.css';
+function App() {
+  return (
+    <div className="container">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App
---
+++ frontend/Taxpal/src/Sign in.jsx
@@ -0,0 +1,27 @@
+function SignIn() {
+  return (
+    <div>
+      <form>
+        <h2>Sign in</h2>
+        <p>Please enter your details to sign in.</p>
+        <div>
+          <label>Username:</label>
+          <input type="Text" placeholder="Username" required />
+        </div>
+        <div>
+        <label>Password:</label>
+          <input type="password" placeholder="Password" required/>
+           <div>
+          <input type="checkbox" />
+          <label>Remember me</label>
+        </div>
+        <a id="f"href="#">Forgot password?</a>
+        </div>
+        <button type="submit">Sign in</button>
+        <p>Don't have an account? <a href="#">Sign up</a></p>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
