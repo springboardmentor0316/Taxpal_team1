ce687da70e1d1a71173b21f04eb7ed8acf564fd9
Moksha Sanghvi <mokshasanghvi9715@gmail.com>
2025-08-24 15:16:29 +0530

    added signup page

--- frontend/Taxpal/src/App.jsx
+++ frontend/Taxpal/src/App.jsx
@@ -1,10 +1,17 @@
 import SignIn from './Sign in.jsx';
+import SignUp from "./Sign Up.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 function App() {
   return (
-    <div className="container">
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
