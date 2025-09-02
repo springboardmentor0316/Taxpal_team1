421e340f6491d0118ebb63f2954e0664601bfd82
dharmikishs <dharmikishs@gmail.com>
2025-09-02 21:11:37 +0530

    WIP: save changes before revert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,30 @@
-
-import SignIn from './Sign in.jsx';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import SignUp from "./Sign Up.jsx";
+import SignIn from "./Sign in.jsx";
+import OtpInput from "./Otp.jsx";
+import VerifyEmail from "./VerifyEmail.jsx";
+import Dashboard from "./Dashboard.jsx";
+import ResetPassword from "./resetPassword.jsx";
+import SetNewPassword from "./SetNewPassword.jsx";
+import "./App.css";
 
-import OtpInput from './Otp.jsx';
-import ResetPassword from './resetPassword.jsx';
-import SetNewPassword from './SetNewPassword.jsx';
-import { VerifyEmail } from './VerifyEmail.jsx';
-import Dashboard from './Dashboard.jsx';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/otp" element={<OtpInput />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/set-new-password" element={<SetNewPassword />} />
-          <Route path="/verify-email" element={<VerifyEmail />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        <ToastContainer />
-      </div>
+      <ToastContainer position="top-right" autoClose={5000} />
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/otp" element={<OtpInput />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/set-new-password" element={<SetNewPassword />} />
+      </Routes>
     </BrowserRouter>
   );
 }
